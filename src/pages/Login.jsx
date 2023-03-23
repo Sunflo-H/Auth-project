7501000a18cc6ba16027d5c3829835c4f70cb424
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-23 18:48:12 +0900

    Feat: "로그인 페이지의 헤더 완성 "
    - Login.jsx 아이콘 추가 및 text class의 위치 이동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styles from "../css/Login.module.css";
+import { BiMessageCheck } from "react-icons/bi";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -8,12 +9,17 @@ export default function Login() {
     navigate("/user/profile");
   };
   return (
-    <div className={styles.main}>
-      <h1 className={styles.text}>Welcome!</h1>
-      <form className={styles["btn-box"]} onSubmit={handleSubmit}>
-        <button className={styles["login-btn"]}>구글 로그인</button>
-        <button className={styles["login-btn"]}>카카오 로그인</button>
-      </form>
+    <div className={styles.body}>
+      <div className={styles.header}>
+        <BiMessageCheck className={styles.icon} />
+        <div className={styles.text}>Welcome Back!</div>
+      </div>
+      <div className={styles.main}>
+        <form className={styles["btn-box"]} onSubmit={handleSubmit}>
+          <button className={styles["login-btn"]}>구글 로그인</button>
+          <button className={styles["login-btn"]}>카카오 로그인</button>
+        </form>
+      </div>
     </div>
   );
 }
