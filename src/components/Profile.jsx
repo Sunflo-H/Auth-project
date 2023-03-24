b8c8ea6b9f6b8747862e3e45ed473198495bc14b
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-24 23:59:32 +0900

    Feat: "프로필 페이지 디자인 수정 및 틀 만듬" #4

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,15 +3,13 @@ import styles from "../css/Profile.module.css";
 
 export default function Profile() {
   return (
-    <div>
-      <div>이미지</div>
-      <div>이름</div>
-      <div className={styles.main}>이메일</div>
-      <div className={styles.main} style={{ backgroundColor: "orange" }}>
-        닉네임
-      </div>
-      <div className={styles.main} style={{ backgroundColor: "orange" }}>
-        패스워드
+    <div className={styles.position}>
+      <div className={styles.main}>
+        <div className={styles["image-box"]}>이미지</div>
+        <div>이름</div>
+        <div>이메일</div>
+        <div style={{ backgroundColor: "orange" }}>닉네임</div>
+        <div style={{ backgroundColor: "orange" }}>패스워드</div>
       </div>
     </div>
   );
