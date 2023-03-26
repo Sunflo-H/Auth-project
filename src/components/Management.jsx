f6939dbec0f5de7682f5344739acc30f60e97aae
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-27 08:19:50 +0900

    Feat: "page 기능 컴포넌트로 분리"

--- src/components/Management.jsx
+++ src/components/Management.jsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import styles from "../css/Management.module.css";
-import {
-  MdOutlineKeyboardArrowRight,
-  MdOutlineKeyboardArrowLeft,
-  MdOutlineKeyboardDoubleArrowLeft,
-  MdOutlineKeyboardDoubleArrowRight,
-} from "react-icons/md";
+
 import { Link } from "react-router-dom";
+import Pagenation from "./Pagenation";
 
 export default function Management() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -17,31 +13,10 @@ export default function Management() {
         <ul className="ul">
           {arr.map((item, i) => {
             if (i > 7) return;
-            return <Card key={i} />;
+            return <Card item={item} key={i} />;
           })}
         </ul>
-
-        <div className={styles["page-box"]}>
-          <div className={styles["page-btn"]}>
-            <MdOutlineKeyboardDoubleArrowLeft />
-          </div>
-          <div className={styles["page-btn"]}>
-            <MdOutlineKeyboardArrowLeft />
-          </div>
-          <ul className={styles["page-num-box"]}>
-            <li className={styles["page-num"]}>1</li>
-            <li className={styles["page-num"]}>2</li>
-            <li className={styles["page-num"]}>3</li>
-            <li className={styles["page-num"]}>4</li>
-            <li className={styles["page-num"]}>5</li>
-          </ul>
-          <div className={styles["page-btn"]}>
-            <MdOutlineKeyboardArrowRight />
-          </div>
-          <div className={styles["page-btn"]}>
-            <MdOutlineKeyboardDoubleArrowRight />
-          </div>
-        </div>
+        <Pagenation />
       </div>
       <Link to="/user/profile" className={styles.profile}>
         내 계정관리
