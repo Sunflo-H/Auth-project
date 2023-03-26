1340f96285b8075e38af3a4e6b30639e123b9198
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-27 08:14:24 +0900

    Feat: "랜덤이미지, 예시 정보 적용" #7
    - 예시용 회원의 프로필 이미지에 랜덤이미지 적용
    - 예시용 회원의 정보 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "../css/Card.module.css";
 
-export default function Card() {
+export default function Card({ item }) {
   return (
     <div className={styles.main}>
-      <div className={styles.image}>이미지</div>
+      <img
+        className={styles.image}
+        src={`https://source.unsplash.com/random/${item}`}
+      ></img>
       <div className={styles["info-box"]}>
-        <div className={styles.name}>이름</div>
-        <div>이메일 </div>
+        <div className={styles.name}>회원 {item}</div>
+        <div>user{item}@sample.com </div>
         <div style={{ backgroundColor: "orange" }}>닉네임 </div>
       </div>
     </div>
